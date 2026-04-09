38a6ccf5b8577d3cd5924d6374e4b698744217a3
clive_mb_air <clivetoniato1@hotmail.com>
2026-04-09 17:54:01 +0100

    lesson 6.1 Serving static files with the fs module

--- second_server/main.js
+++ second_server/main.js
@@ -1,36 +1,33 @@
 // curl --data "username= Jon&password=secret" http://localhost:3000
-// lesson 5.3. Adding routes to a web application
-
-// Define mapping of routes with responses.
-const routeResponseMap = {                              
-  "/info": "<h1>Info Page</h1>",
-  "/contact": "<h1>Contact Us</h1>",
-  "/about": "<h1>Learn More About Us.</h1>",
-  "/hello": "<h1>Say hello by emailing us <a href='mailto:email@example.com'>here</a></h1>",
-  "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
-};
+// lesson 6.1. Serving static files with the fs module
 
 const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
-  app = http.createServer((req, res) => {
+  // Require the fs module.
+  fs = require("fs");
+  // Set up route mapping for HTML files.
+const routeMap = {
+  "/": "views/index.html",
+};
+
+http
+  .createServer((req, res) => {
     res.writeHead(httpStatus.StatusCodes.OK, {
       "Content-Type": "text/html",
-    })
-    // Check whether a request route is defined in the map.
-    if(req.url =="/error"){
-  res.end(`${httpStatus.StatusCodes.NOT_FOUND} ${routeResponseMap[req.url]}`)
-      console.log(req.url)
-    }
-   else if(routeResponseMap[req.url]){
-   res.end(routeResponseMap[req.url])
-      console.log(req.url)
+    });
+    if (routeMap[req.url]) {
+      // Read the contents of the mapped file.
+      fs.readFile(routeMap[req.url], (err, data) => {
+        // Respond with file contents.
+        res.write(data);
+        res.end();
+      });
     } else{
-      // Respond with default HTML.
-      res.end("<h1>Welcome!</h1>");
-    }  
-  });
-
-app.listen(port);
+      res.end("<h1>Sorry, not found.</h1>");
+// console.log(req.url=='/')
+    }
+  })
+  .listen(port);
 console.log(`The server has started and is listening on port number:
 ${port}`);
