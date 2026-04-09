bb1f88a1b3f938cd87575173002e5bbb18f0c21d
clive_mb_air <clivetoniato1@hotmail.com>
2026-04-09 16:42:12 +0100

    lesson 5.3. Adding routes to a web application extended

--- second_server/main.js
+++ second_server/main.js
@@ -1,31 +1,30 @@
 // curl --data "username= Jon&password=secret" http://localhost:3000
-// lesson 5.2 analyzing request data
-const getJSONString = obj =>{
-      return JSON.stringify(obj,null,2)
-    }
+// lesson 5.3. Adding routes to a web application
+
+// Define mapping of routes with responses.
+const routeResponseMap = {                              
+  "/info": "<h1>Info Page</h1>",
+  "/contact": "<h1>Contact Us</h1>",
+  "/about": "<h1>Learn More About Us.</h1>",
+  "/hello": "<h1>Say hello by emailing us here</h1>",
+  "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+};
+
 const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
-  app = http.createServer();
-  app.on('request',(req, res) => {
-    let body = []
-    req.on("data", (bodyData)=>{
-      body.push(bodyData)
-    })
-    req.on("end",()=>{
-      body = Buffer.concat(body).toString();
-      console.log(`Request Body Contents: ${body}`)
-    })
-    console.log(`URL: ${getJSONString(req.url)}`)
-    console.log(`Headers: ${getJSONString(req.headers)}`)
-    console.log(`Method: ${getJSONString(req.method)}`)
+  app = http.createServer((req, res) => {
     res.writeHead(httpStatus.StatusCodes.OK, {
       "Content-Type": "text/html",
     })
-    
-    let responseMessage = "<h1>Hello, Universe!</h1>";
-    res.end(responseMessage);
-    console.log(`Sent a response : ${responseMessage}`);
+    // Check whether a request route is defined in the map.
+    if(routeResponseMap[req.url]){
+      res.end(routeResponseMap[req.url])
+      console.log(req.url)
+    } else{
+      // Respond with default HTML.
+      res.end("<h1>Welcome!</h1>");
+    }  
   });
 
 app.listen(port);
