dfb17ac29ab73943f15370b143cdb80bb4e04881
clive_mb_air <clivetoniato1@hotmail.com>
2026-04-09 21:42:53 +0100

    lesson 6.2. Serving assets

--- second_server/main.js
+++ second_server/main.js
@@ -3,42 +3,84 @@
 
 
 
-// Create a function to interpolate the URL into the file path.
-const getViewUrl = (url) => {     
-  if(url=='/'){
-    return `views/index.html`; 
-  }             
-  return `views${url}.html`;
+const http = require("http"),
+  httpStatus = require("http-status-codes"),
+  fs = require("fs");
+
+// 1 Create an error-handling function.
+const sendErrorResponse = (res) => {
+  res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
+    "Content-Type": "text/html",
+  });
+  res.write("<h1>FILE NOT FOUND</h1>");
+  res.end();
 };
 
-const port = 3000,
-  http = require("http"),
-  httpStatus = require("http-status-codes"),
-  // Require the fs module.
-  fs = require("fs");
-  // Set up route mapping for HTML files.
 
 
 http
   .createServer((req, res) => {
-    // Get the file-path string. 
-   let viewUrl = getViewUrl(req.url);
-  //  Interpolate the request URL into your fs file search. 
-   fs.readFile(viewUrl, (error,data)=>{
-    // Handle errors with a 404 response code.
-    if(error){
-      res.writeHead(httpStatus.StatusCodes.NOT_FOUND);
-      res.write("<h1>FILE NOT FOUND</h1>");
-    } else{
-      // Respond with file contents.
-      res.writeHead(httpStatus.StatusCodes.OK,{
-              "Content-type": "text/html"
-            });
-      res.write(data)
+    // 2 Store the request’s URL in a variable url.
+    let url = req.url;
+    console.log(url)
+    /// 3 Check the URL to see whether it contains a file extension.
+    if (url.indexOf(".html") !== -1) {
+      res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "text/html",
+      });
+      // Customize the response’s content type.
+      // Call readFile to read file contents.
+      customReadFile(`./views${url}`, res);
+    } 
+    else if (url.indexOf(".js") !== -1) {
+      res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "text/javascript",
+      });
+      customReadFile(`./public/js${url}`, res);
+
     }
-    res.end()
-   })
+     else if (url.indexOf(".css") !== -1) {
+      res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "text/css",
+      });
+      customReadFile(`./public/css${url}`, res);
+
+    } 
+    else if (url.indexOf(".png") !== -1) {
+      res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "image/png",
+      });
+      customReadFile(`./public/images${url}`, res);
+
+    } 
+    else {
+      sendErrorResponse(res);
+
+    }
   })
-  .listen(port);
+  .listen(3000);
 console.log(`The server has started and is listening on port number:
-${port}`);
+${3000}`);
+
+// Look for a file by the name requested.
+const customReadFile=(file_path,res)=>{
+  // // 7 Check whether the file exists.
+  if(fs.existsSync(file_path)){
+
+    fs.readFile(file_path,(err,data)=>{
+      if(err){
+        console.log(err)
+        onsole.log("doesnt exists")
+        sendErrorResponse(res)
+        return
+      }
+      console.log("exists")
+      res.write(data);
+      res.end()
+      return
+    });
+  } else{
+    sendErrorResponse(res)
+    return
+  }
+}
