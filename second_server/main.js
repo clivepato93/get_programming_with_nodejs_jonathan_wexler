9f387b05579a4efe9bc943ceb9f4574f991dae71
clive_mb_air <clivetoniato1@hotmail.com>
2026-04-10 18:33:53 +0100

    lesson 6.3. Moving your routes to another file

--- second_server/main.js
+++ second_server/main.js
@@ -1,86 +1,48 @@
 // curl --data "username= Jon&password=secret" http://localhost:3000
-// lesson 6.1. Serving static files with the fs module
+// lesson 6.3. Moving your routes to another file
 
+const port = 3000,
+  http = require("http"),
+  httpStatusCodes = require("http-status-codes"),
+  router = require("./router"),
+  fs = require("fs"),
+  plainTextContentType = {
+    "Content-Type": "text/plain"
+  },
+  htmlContentType = {
+    "Content-Type": "text/html"
+  },
+  // Create a custom readFile function to reduce code repetition.
 
-
-const http = require("http"),
-  httpStatus = require("http-status-codes"),
-  fs = require("fs");
-
-// 1 Create an error-handling function.
-const sendErrorResponse = (res) => {
-  res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
-    "Content-Type": "text/html",
-  });
-  res.write("<h1>FILE NOT FOUND</h1>");
-  res.end();
-};
-
-
-
-http
-  .createServer((req, res) => {
-    // 2 Store the request’s URL in a variable url.
-    let url = req.url;
-    console.log(url)
-    /// 3 Check the URL to see whether it contains a file extension.
-    if (url.indexOf(".html") !== -1) {
-      res.writeHead(httpStatus.StatusCodes.OK, {
-        "Content-Type": "text/html",
-      });
-      // Customize the response’s content type.
-      // Call readFile to read file contents.
-      customReadFile(`./views${url}`, res);
-    } 
-    else if (url.indexOf(".js") !== -1) {
-      res.writeHead(httpStatus.StatusCodes.OK, {
-        "Content-Type": "text/javascript",
-      });
-      customReadFile(`./public/js${url}`, res);
-
-    }
-     else if (url.indexOf(".css") !== -1) {
-      res.writeHead(httpStatus.StatusCodes.OK, {
-        "Content-Type": "text/css",
-      });
-      customReadFile(`./public/css${url}`, res);
-
-    } 
-    else if (url.indexOf(".png") !== -1) {
-      res.writeHead(httpStatus.StatusCodes.OK, {
-        "Content-Type": "image/png",
-      });
-      customReadFile(`./public/images${url}`, res);
-
-    } 
-    else {
-      sendErrorResponse(res);
-
-    }
-  })
-  .listen(3000);
-console.log(`The server has started and is listening on port number:
-${3000}`);
-
-// Look for a file by the name requested.
-const customReadFile=(file_path,res)=>{
-  // // 7 Check whether the file exists.
-  if(fs.existsSync(file_path)){
-
-    fs.readFile(file_path,(err,data)=>{
-      if(err){
-        console.log(err)
-        onsole.log("doesnt exists")
-        sendErrorResponse(res)
-        return
+  customReadFile = (file, res) => {  // 
+    fs.readFile(`./${file}`, (errors, data) => {
+      if (errors) {
+        console.log("Error reading the file...");
       }
-      console.log("exists")
-      res.write(data);
-      res.end()
-      return
+      res.end(data);
     });
-  } else{
-    sendErrorResponse(res)
-    return
-  }
-}
+  };
+
+router.get("/", (req, res) => {  // Register routes with get and post.
+  res.writeHead(httpStatusCodes.StatusCodes.OK, plainTextContentType);
+  res.end("INDEX");
+});
+router.get("/index.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+
+router.get("/sample.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/sample.html", res);
+});
+
+router.post("/", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, plainTextContentType);
+  res.end("POSTED");
+});
+
+// Handle all requests through router.js.
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number:
+ ${port}`);
