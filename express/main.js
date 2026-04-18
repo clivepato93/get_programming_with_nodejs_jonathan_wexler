97996c39a5109af41806c69dc94cebed5ab98510
clive_mb_air <clivetoniato1@hotmail.com>
2026-04-18 18:41:26 +0100

    8.3

--- express/main.js
+++ express/main.js
@@ -3,7 +3,11 @@ express = require('express')
 app = express()
 
 app.get('/',(req,res)=>{
-    console.log(req.url);
+   console.log(req.params);         1
+console.log(req.body);
+console.log(req.url);
+console.log(req.query);
+
     res.send("testing")
 
 }).listen(port,()=>console.log("listening on port: "+ port))
